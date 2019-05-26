fc2e0331cb9adff4515caacc4de0156aefd3bd21
put-your-name-here <put-your-email-here>
2019-05-26 12:41:58 +0800

    使用koa-views

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
 const Koa = require('koa');
 const app = new Koa();
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl');
-const ejs = require('ejs');
-const pug = require('pug');
+const views = require('koa-views');
+const { resolve } = require('path');
 
+app.use(
+  views(resolve(__dirname, './views'), {
+    extension: 'pug'
+  })
+);
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html;charset=utf-8';
-  //   ctx.body = htmlTpl;
-  //   ctx.body = ejs.render(ejsTpl, {
-  //     you: 'Koa',
-  //     me: 'Koa'
-  //   });
-  ctx.body = pug.render(pugTpl, {
-    you: 'Koa',
-    me: 'Koa'
+  await ctx.render('index', {
+    you: 'Kao',
+    me: 'koa'
   });
 });
 
