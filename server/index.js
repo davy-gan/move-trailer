fa83d20c0e92310697a9019468a7d69d39bb396b
put-your-name-here <put-your-email-here>
2019-05-26 12:09:42 +0800

    模板引擎使用

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl');
+const ejs = require('ejs');
+const pug = require('pug');
 
-app.use(async (ctx,next)=>{
-    ctx.body ="电音首页"
-})
+app.use(async (ctx, next) => {
+  ctx.type = 'text/html;charset=utf-8';
+  //   ctx.body = htmlTpl;
+  //   ctx.body = ejs.render(ejsTpl, {
+  //     you: 'Koa',
+  //     me: 'Koa'
+  //   });
+  ctx.body = pug.render(pugTpl, {
+    you: 'Koa',
+    me: 'Koa'
+  });
+});
 
-app.listen(2333)
+app.listen(2333);
